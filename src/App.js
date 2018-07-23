8997eea5b4457d47c34b9f6c556b9c17263d9b60
simenghe <simengschool@gmail.com>
2018-07-23 00:13:54 -0400

    First one

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <code>What is good homies</code>
+        </p>
+          <header className="Big-chad">
+              <h1 className="bigger cane"> <code>Whats up</code></h1>
+          </header>
+      </div>
+    );
+  }
+}
+
+export default App;
