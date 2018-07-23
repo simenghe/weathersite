7f4177004fa6d23f02a4d6a42d6d3888671c854e
simenghe <simengschool@gmail.com>
2018-07-23 18:38:19 -0400

    New file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./components/Weather.js"
+const API_KEY="da11038c911fa7bfbc83fcc237df37e0"
 
-class App extends Component {
+class App extends React.Component{
+    getWeather=async()=>{
+        const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+        const data=await api_call.json();
+        console.log(data);
+    }
   render() {
-      const stringer="askdojalskdjklsajdlkasdkjlasdklasdkljlkj"
     return (
       <div className="App">
         <header className="App-header">
@@ -12,10 +18,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <code>What is good homies</code>
+          <code>wez</code>
         </p>
           <header className="Big-chad">
-              <h1 className="bigger cane"><code>Whats up</code></h1>
+              <h1 className="App-intro"><code>Whats up</code></h1>
+              <h2 className="Big-chad"><code>Bigger chad</code></h2>
           </header>
       </div>
     );
