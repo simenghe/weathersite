db284d206f8f3454bfb8e4a44c19a252ceffb5bb
simenghe <simengschool@gmail.com>
2018-08-31 04:57:53 +0800

    New file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Weather from "./components/Weather.js"
-const API_KEY="da11038c911fa7bfbc83fcc237df37e0"
+import React, { Component, Card } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./components/Weather.js";
+import ChadCard from "./components/ChadCard.js";
+const API_KEY = "da11038c911fa7bfbc83fcc237df37e0";
+const diaboloImage='http://medias12.netjuggler.net/produit/epic-diabolo/a-dream-3.jpg';
 
-class App extends React.Component{
-    getWeather=async()=>{
-        const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
-        const data=await api_call.json();
-        console.log(data);
-    }
+class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my website!</h1>
         </header>
         <p className="App-intro">
-          <code>wez</code>
+            <ChadCard/>
+            <img src={diaboloImage}/>
+          <code>Integrate the wez</code>
         </p>
-          <header className="Big-chad">
-              <h1 className="App-intro"><code>Whats up</code></h1>
-              <h2 className="Big-chad"><code>Bigger chad</code></h2>
-          </header>
+        <header className="Big-chad">
+          <h1 className="App-intro">
+            <code>Whats up</code>
+          </h1>
+          <h2 className="Big-chad">
+            <code>Bigger chad</code>
+          </h2>
+            <h3 className="Big-title">
+                MY HACKINTOSH JOURNEY!
+            </h3>
+        </header>
       </div>
     );
   }
---
+++ src/components/ChadCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import { Card, CardHeader, CardBody, CardFooter } from "react-simple-card";
+
+class ChadCard extends React.Component {
+  render() {
+    return (
+      <form>
+        <Card Height="123">
+          <div>Biggest Chad</div>
+        </Card>
+      </form>
+    );
+  }
+}
+export default ChadCard;
